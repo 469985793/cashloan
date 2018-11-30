29851155a91165aa14734bc6580b15864dec5a64
alan <15237815570@126.com>
2018-11-30 17:15:22 +0800

    人工复审加入借款订单信息

---
+++ cashloan-manage/src/main/react/modules/RiskControlManage/LoanApplicationManage/Components/Tab6.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import {
+  Table,
+} from 'antd';
+const objectAssign = require('object-assign');
+var Tab6 = React.createClass({
+  getInitialState() {
+    return {
+      loading: false,
+      data: [],
+      pagination: {},
+    };
+  },
+  rowKey(record) {
+    return record.uid;
+  },
+  componentWillReceiveProps(nextProps){
+    if(nextProps.activeKey == '7'){
+      this.fetch();
+    }
+  },
+  componentDidMount(){
+    this.fetch();
+  },
+  handleTableChange(pagination, filters, sorter) {
+    const pager = this.state.pagination;
+    pager.current = pagination.current;
+    pager.userId = this.props.record.uid,
+    this.setState({
+      pagination: pager,
+    });
+    this.fetch(pager);
+  },
+  fetch(params = {}) {
+    console.log(123);
+    this.setState({
+      loading: true
+    });
+    if (!params.pageSize) {
+      var params = {};
+      params = {
+        pageSize: 5,
+        current: 1,
+        userId: this.props.record.uid,
+      }
+    }
+    Utils.ajaxData({
+      url: '/modules/manage/borrow/listBorrowLog.htm',
+      data: params,
+      callback: (result) => {
+        console.log(result)
+        const pagination = this.state.pagination;
+        pagination.current = params.current;
+        pagination.pageSize = params.pageSize;
+        pagination.total = result.page.total;
+        if (!pagination.current) {
+          pagination.current = 1
+        };
+        this.setState({
+          loading: false,
+          data: result.data.list,
+          pagination
+        });
+      }
+    });
+  },
+  render() {
+    var columns = [{
+      title: '订单号',
+      dataIndex: "indentNo",
+    }, {
+      title: '借款金额（元）',
+      dataIndex: "balance",
+    }, {
+      title: '借款期限（天）',
+      dataIndex: "cycle",
+    }, {
+      title: '借款时间',
+      dataIndex: "createdTime",
+    }, {
+      title: '综合费用（元）',
+      dataIndex: "fee",
+    }, {
+      title: '实际到账金额（元）',
+      dataIndex: "actualBalance",
+    }, {
+      title: '实际还款金额（元）',
+      dataIndex: "actualbackAmt",
+    }, {
+      title: '状态',
+      dataIndex: "status",
+    }];
+    return (<div className="block-panel">
+              <Table columns={columns} rowKey={this.rowKey}  
+              dataSource={this.state.data}
+              pagination={this.state.pagination}
+              loading={this.state.loading}
+              onChange={this.handleTableChange}  />
+          </div>
+    );
+  }
+});
+export default Tab6;
