00287adbe41031a20faa19510c7307e08bc08bf8
louzenghui <liuzhe8760@adpanshi.com>
2018-12-11 16:35:01 +0800

    还款管理->查询还款计划,还款列表添加时间选择框

--- cashloan-manage/src/main/react/modules/RepaymentManage/RepaymentPlanList/Components/SeachForm.js
+++ cashloan-manage/src/main/react/modules/RepaymentManage/RepaymentPlanList/Components/SeachForm.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Button, Form, Input, Select, Message } from 'antd';
+import { Button, Form, Input, Select, Message,DatePicker } from 'antd';
 const createForm = Form.create;
 const FormItem = Form.Item;
 const Option = Select.Option;
-
+const RangePicker = DatePicker.RangePicker;
 let SeachForm = React.createClass({
     getInitialState() {
         return {
@@ -12,6 +12,8 @@ let SeachForm = React.createClass({
     },
     handleQuery() {
         var params = this.props.form.getFieldsValue();
+        params.beforeTime=params.registTime[0];
+        params.afterTime=params.registTime[1];
         this.props.passParams({
             searchParams: JSON.stringify(params),
             pageSize: 10,
@@ -27,14 +29,21 @@ let SeachForm = React.createClass({
     },
     handleOut() {
         var params = this.props.form.getFieldsValue();
+        params.beforeTime=params.registTime[0];
+        params.afterTime=params.registTime[1]; 
         var json = JSON.stringify(params);
         window.open("/modules/manage/repaymentPlanList/export.htm?searchParams="+encodeURI(json));
 
     },
+    disabledDate(startValue){
+        var today = new Date();
+        return startValue.getTime() > today.getTime();
+        // return current && current < moment().endOf('day');
+    },
     render() {
 
         const { getFieldProps } = this.props.form;
-
+        const date = new Date();
         return (
             <Form inline>
                 <FormItem label="Real Name:">{/*真实姓名*/}
@@ -45,6 +54,9 @@ let SeachForm = React.createClass({
                 </FormItem>
                 <FormItem label="Order Number:">{/*订单号*/}
                     <Input  {...getFieldProps('orderNo') } />
+                </FormItem>
+                <FormItem label="Date：">
+                    <RangePicker disabledDate={this.disabledDate} style={{ width: "310" }} {...getFieldProps('registTime', { initialValue: [date,date] })} />
                 </FormItem>
                 <FormItem label="Repayment Status:">{/*还款状态*/}
                     <Select style={{ width: 150 }} {...getFieldProps('state', { initialValue: '' }) }>
